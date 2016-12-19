15599d2903d5b6f6ad4dd245e0d235d543326814
Alan Hietala <alan.hietala@gmail.com>
2016-12-19 00:59:21 -0500

    fixing error in data object

--- app/js/data/data.js
+++ app/js/data/data.js
@@ -3,7 +3,7 @@ export default [
 		identifier:'318493836284', 
 		name: 'Main Floor',
 		currentTemperature: 72,
-		desiredTemperature: 74
+		desiredTemperature: 74,
 		desiredTempMax: 82,
 		desiredTempMin: 62
 	},
@@ -11,7 +11,7 @@ export default [
 		identifier:'314636447732', 
 		name: 'Upstairs',
 		currentTemperature: 74,
-		desiredTemperature: 78
+		desiredTemperature: 78,
 		desiredTempMax: 82,
 		desiredTempMin: 62
 	}
